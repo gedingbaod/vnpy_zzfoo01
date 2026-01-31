4da2fd76e6a6717ab229e7021f66cc81984df735
gedingbaod <3230332561@qq.com>
2026-01-31 22:49:08 +0800

    gm导入tick数据

--- mycode/download_tick_from_gm.py
+++ mycode/download_tick_from_gm.py
@@ -1,8 +1,12 @@
 # 忽略各模块的警告信息
+import sys
 import warnings
 
 import pandas as pd
-# from rqdatac.services.basic import instruments
+from gm.api import set_token, get_history_symbol, get_previous_n_trading_dates
+from gm.api._utils import _timestamp_to_str
+from gm.pb.instrument_service_pb2 import GetTradingDatesPrevNResp, GetTradingDatesPrevNReq
+from gm.pb_to_dict import protobuf_to_dict
 
 from common.file_dir import traverse_file_in_dir, load_data_from_csv
 
@@ -11,10 +15,9 @@ warnings.filterwarnings("ignore")
 from datetime import datetime
 
 from vnpy.trader.datafeed import get_datafeed
-from vnpy.trader.database import get_database, DB_TZ
+from vnpy.trader.database import get_database
 from vnpy.trader.constant import Interval, Exchange
-from vnpy.trader.object import BarData, HistoryRequest, TickData
-from vnpy.trader.utility import extract_vt_symbol
+from vnpy.trader.object import TickData, HistoryRequest
 from vnpy.trader.setting import SETTINGS
 
 # 交易所映射
@@ -27,18 +30,12 @@ EXCHANGE_CTP2VT: dict[str, Exchange] = {
     "GFEX": Exchange.GFEX
 }
 
-# 配置数据服务
-# SETTINGS["datafeed.name"] = "rqdata"            # 可以根据自己的需求选择数据服务：rqdata/xt/wind等
-# SETTINGS["datafeed.username"] = "license"       # RQData的用户名统一为“license”这个字符串
-# SETTINGS["datafeed.password"] = "dsSCpoxgtMiI4lkILRFJncLB3eMttaiqIhDstum0fTRrVCG5tjoDfv_2-LikVPXvG5jMkduQlRGUASm1_2Bs3hE9tmGCGlUObADeEY5GyeAP3y-Ggrd0ebO7L8e-s3J16EFZW7xRJyu-rNgysG8dVHRBOrdL1ZHUDkLJpsKhZlU=XZh2iFOUVV1hk-JhpZ4seKUSGDMTzUQBOsOPP_Gxf3H-zpa1KjQPiQrw01d9aXh__qForIPMsUDJ6WSerVqYXQhwZoNnkuiHeyQ-Md43j1f5GfAV5L2q_AOdPzcl6cMJRSdykN9niwLRz0fsHrPP91TO29SZ2MwCDQ9nYA3JD3w="        # 这里需要替换为你购买或者申请试用的RQData数据license
-
 SETTINGS["datafeed.name"] = "gm"
 SETTINGS["datafeed.username"] = ""
 SETTINGS["datafeed.password"] = "0dc802db1a918f7c064237478e69b83a024028e7"
 
-
 # 配置数据库
-SETTINGS["database.name"] = "dolphindb"              # 可以根据自己的需求选择数据库，这里使用的是
+SETTINGS["database.name"] = "dolphindb"
 SETTINGS["database.database"] = "vnpy"
 SETTINGS["database.host"] = "127.0.0.1"
 SETTINGS["database.port"] = 8848
@@ -58,6 +55,51 @@ def log_write(msg):
     log_file.write(out_str)
     print(out_str)
     log_file.flush()
+
+def get_dates_from_range(start_date: str, end_date: str)-> list:
+
+    date_df = get_history_symbol(symbol='CFFEX.IC', start_date=start_date, end_date=end_date, df= True)
+
+    if date_df.empty:
+        return []
+
+    if pd.api.types.is_datetime64_any_dtype(date_df['trade_date']):
+        date_list = date_df['trade_date'].dt.strftime('%Y-%m-%d').tolist()
+    else:
+        # 如果是字符串类型，先转换为 datetime 再格式化
+        date_list = pd.to_datetime(date_df['trade_date']).dt.strftime('%Y-%m-%d').tolist()
+
+    return date_list
+
+def set_specific_time(dt_obj, hour=0, minute=0, second=0):
+    """
+    修改datetime对象的时间部分
+    参数:
+        dt_obj (datetime): 原始datetime对象
+        hour (int): 目标小时(0-23)
+        minute (int): 目标分钟(0-59)
+        second (int): 目标秒(0-59)
+    返回:
+        datetime: 新时间点的datetime对象
+    """
+    return dt_obj.replace(hour=hour, minute=minute, second=second, tzinfo=None)
+
+def get_trading_datetime(base_date: datetime)-> (datetime, datetime):
+    """
+    计算给定日期的下一日
+    参数:
+        date_str (str): 格式为'YYYY-MM-DD'的日期字符串
+    返回:
+        str: 下一日的日期字符串，格式为'YYYY-MM-DD'
+    """
+    # 获取前一个交易日
+    start_time_list = get_previous_n_trading_dates(exchange='CFFEX', date=base_date.strftime('%Y-%m-%d'))
+    # start_time = base_date - timedelta(days=1)
+    # 交易开始时间   交易日开始的前一天晚上
+    start_time = set_specific_time(datetime.strptime(start_time_list[0], "%Y-%m-%d"), 20, 0, 0)
+    # 交易结束时间   这一天是交易日
+    end_time = set_specific_time(base_date, 16, 0, 0)
+    return start_time, end_time
 
 def main(csv_path):
     result: bool = datafeed.init()
@@ -89,17 +131,24 @@ def main(csv_path):
             # Timestamp转为datetime，测试过没有损失精度
             listed_date = row['listed_date'].to_pydatetime()
             delisted_date = row['delisted_date'].to_pydatetime()
+            listed_date = listed_date.replace(tzinfo=None)
+            delisted_date = delisted_date.replace(tzinfo=None)
 
-            # current_time = datetime(2026, 1, 30)
-            local_time = pd.Timestamp('2026-01-30', tz='Asia/Shanghai')
+            local_time = datetime(2026, 1, 30)
+
 
             if delisted_date > local_time:
+                local_time_str = local_time.strftime("%Y-%m-%d")
+                date_list = get_dates_from_range(local_time_str, local_time_str)
+                # if len(date_list) == 0:
+                #     sys.exit(1)
+                start_time, end_time = get_trading_datetime(local_time)
 
                 # 打印行数据（可替换为你的业务逻辑）
                 # log_write(f"索引：{idx+1} 总数：{total} | 交易所：{exchange} "
                 #           f"| 合约代码：{sec_id} | 上市日期：{listed_date} | 退市日期：{delisted_date}")
 
-                vnpy_tick_process(sec_id, exchange, local_time, local_time, Interval.TICK)
+                vnpy_tick_process(sec_id, exchange, start_time, end_time, Interval.TICK)
 
                 # if not check_loaded(symbol=sec_id, exchange=exchange, list_loaded=list_loaded):
                 #     log_write(f"{sec_id}.{exchange} 未下载")
@@ -137,8 +186,8 @@ def vnpy_tick_process(symbol, exchange, start, end, interval):
 
 
 if __name__ == "__main__":
-    main(r'D:\02.QUANT\QuantData\DataFeedEx\juejin\data\test')
-    # main(r'I:\QUANT\QuantData\DataFeedEx\juejin\data\contracts_csv_202601')
+    # main(r'D:\02.QUANT\QuantData\DataFeedEx\juejin\data\test')
+    main(r'I:\QUANT\QuantData\DataFeedEx\juejin\data\contracts_csv_202601')
     pass
 
 
