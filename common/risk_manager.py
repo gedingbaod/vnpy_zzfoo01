e6805dc5a2b4338bdc0f4089145149364532a4b5
gedingbaod <3230332561@qq.com>
2026-03-06 08:12:04 +0800

    修改打印格式

--- common/risk_manager.py
+++ common/risk_manager.py
@@ -439,10 +439,13 @@ def output_position(position: dict, is_current = False) -> list[str]:
 def str_indicators(spread_indicator: tuple) -> str:
     if spread_indicator is None:
         return "无"
-    (mean, std, upper_bound, lower_bound, quote_space_spread, final_limit) = spread_indicator
-    mean = round(mean, 2)
-    std = round(std, 2)
-    return f'均值：{mean}，方差：{std}，上轨：{upper_bound}，下轨：{lower_bound}, 盘口成本：{quote_space_spread}，最终差价取值：{final_limit}'
+    if len(spread_indicator) == 6:
+        (mean, std, upper_bound, lower_bound, quote_space_spread, final_limit) = spread_indicator
+        mean = round(mean, 2)
+        std = round(std, 2)
+        return f'均值：{mean}，方差：{std}，上轨：{upper_bound}，下轨：{lower_bound}, 盘口成本：{quote_space_spread}，最终差价取值：{final_limit}'
+    else:
+        return spread_indicator
 
 
 if __name__ == "__main__":
