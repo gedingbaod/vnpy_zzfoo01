08283e9c81bdbe2b0018ec064b6dc51742a8e9a3
gedingbaod <3230332561@qq.com>
2026-03-09 21:18:29 +0800

    减少延迟时间

--- common/time_delay.py
+++ common/time_delay.py
@@ -13,8 +13,8 @@ NS_005959 = 3599900000000
 NS_125900 = 46740000000000
 NS_125959 = 46799900000000
 
-# NS_NOTE_BOOK_BIAS = 161200 * (10 ** 3)
-NS_NOTE_BOOK_BIAS = 240000 * (10 ** 3)
+NS_NOTE_BOOK_BIAS = 161200 * (10 ** 3)
+# NS_NOTE_BOOK_BIAS = 200000 * (10 ** 3)
 
 def check_market_opening_time_morning() -> bool:
     """
